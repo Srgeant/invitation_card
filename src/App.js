273bbf35757821a6e330f9167a5e114e69f640e3
Sergey <eloyan2013@gmail.com>
2024-11-09 10:28:49 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
 import './App.css';
 import Wedding from './Wedding';
+import { useEffect } from 'react';
 
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Spartak & Nare's Wedding Invitation"
+  })
+
   return (
     <div className="App">
       <Wedding />
